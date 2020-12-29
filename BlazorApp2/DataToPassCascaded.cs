02d7c25386e0e810f8f7d9356e59faf3d16d5e7d
Andy Jones <uez5185@live.com>
2020-12-29 19:55:38 +0000

    Add project files.

---
+++ BlazorApp2/DataToPassCascaded.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp2
+{
+    public class DataToPassCascaded
+    {
+        public string StringData { get; set; }
+        public int IntData { get; set; }
+    }
+}
